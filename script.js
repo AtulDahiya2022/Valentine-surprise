453cd14706de15c87929e43b76faeaf8d7ede109
AtulDahiya2022 <103523707+AtulDahiya2022@users.noreply.github.com>
2023-02-13 20:01:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var audio = new Audio('Tum Se Hi 128 Kbps.mp3');
+document.addEventListener("DOMContentLoaded", function() {
+    const message = document.querySelector("#message");
+    const yesButton = document.querySelector("#yes");
+    const noButton = document.querySelector("#no");
+  
+    message.textContent = "Will you be mine forever?";
+  
+    yesButton.addEventListener("click", function() {
+      const ele = document.getElementById('done');
+      ele.style.backgroundImage = "url('giphy.webp')";
+      const para = document.getElementById('message');
+      para.innerHTML = "You are mine Forever";
+      para.style.color = '#fff';
+      
+      audio.play();
+      alert("Congratulations! you are totally mine for today!!"   );
+    });
+  
+    noButton.addEventListener("click", function() {
+      alert("RHEGI TO FIRBHI MERI HI");
+    });
+  });
+  
